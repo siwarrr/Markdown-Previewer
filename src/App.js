84833f9ab150cef9404e74ba17b2131f7f260692
siwarrr <bensiwar@gmailcom>
2024-11-21 22:13:09 +0100

    markdown previewer first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState(`# Welcome to Markdown Previewer!
+
+## This is a sub-heading...
+
+- Here's a list item
+- **Bold text**
+- [A link](https://fr.legacy.reactjs.org/)
+
+\`\`\`
+function example() {
+  return "Code block example";
+}
+\`\`\`
+
+> Blockquote example
+
+![Image Example](https://tse1.mm.bing.net/th?id=OIP.K-4RqDC6zFrpAG31ayDDOgHaHa&pid=Api&P=0&h=180)
+`);
+
+  const handleMarkdownChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='app'>
+    <div className='container'>
+      <div className='header'>
+        <span>Editor</span>
+        <button className='close-button' onClick={ () => setMarkdown("")}>✖</button>
+      </div>
+    <textarea
+      id='editor'
+      value={markdown}
+      onChange={handleMarkdownChange}
+      placeholder='Enter Markdown here...'
+    />
     </div>
+    <div className='container'>
+      <div className='header'>
+        <span>Previewer</span>
+        <button className='close-button' onClick={ () => setMarkdown("")}>✖</button>
+      </div>
+    <div 
+      id='preview'
+      dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      ></div>
+    </div>
+   </div> 
   );
 }
 
